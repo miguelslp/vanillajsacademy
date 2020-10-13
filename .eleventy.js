f855bde1b927fda0ac03d3b85aa5ddc73130bc6b
Miguel Pinto <miguelslpinto@gmail.com>
2020-10-13 22:10:38 +0100

    debugging github actions

--- .eleventy.js
+++ .eleventy.js
@@ -65,6 +65,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy(".github");
 
   /* Markdown Overrides */
   let markdownLibrary = markdownIt({
