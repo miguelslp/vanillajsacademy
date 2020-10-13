f561d5522aa6e8aeeedd7f34e0a1f4ad085e6c31
Miguel Pinto <miguelslpinto@gmail.com>
2020-10-13 21:05:09 +0100

    Add github actions

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 
 module.exports = function(eleventyConfig) {
+  pathPrefix: "/vanillajsacademy/";
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
