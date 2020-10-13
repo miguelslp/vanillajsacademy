49af0ac12e722b436314b7d3ecc0802875496093
Miguel Pinto <miguelslpinto@gmail.com>
2020-10-13 22:24:16 +0100

    fixing action bug

--- .eleventy.js
+++ .eleventy.js
@@ -65,8 +65,6 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy(".github");
-  eleventyConfig.addPassthroughCopy("_site");
 
   /* Markdown Overrides */
   let markdownLibrary = markdownIt({
